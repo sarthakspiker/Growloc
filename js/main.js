79b515ad06d599b7f2d93294e5155a7873a29e72
prathamesh <kulkarniprathamesh03@gmail.com>
2022-12-12 19:16:28 +0530

    First phase

---
+++ js/main.js
@@ -0,0 +1,59 @@
+
+// header scroll
+
+
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 50) {
+    nav.classList.add("header-scrolled");
+  } else {
+    nav.classList.remove("header-scrolled");
+  }
+};
+
+// nav hide
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function (e) {
+  e.addEventListener("click", function () {
+    navCollapse.classList.remove("show");
+  });
+});
+
+
+//homepage gallery slider
+$('.responsive').slick({
+    dots: true,
+      prevArrow: $('.prev'),
+      nextArrow: $('.next'),
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+  
+    ]
+  });
